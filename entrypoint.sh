1ffb549d0cf529a4cc086226d3202217a3af65e9
Han Xiao <han.xiao@jina.ai>
2020-08-25 13:01:20 +0200

    feat: add action yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,16 @@ set -ex
 pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${pull_number}/files"
 FILES=$(curl -s -X GET -G $URL | jq -c '.[] | select( .filename | endswith("manifest.yml")) | .filename | rtrimstr("manifest.yml")')
+
+rc=0
 if [ -z "$FILES" ]
 then
       echo "nothing to build"
 else
-      echo "something to build:"
+      echo "something to build: $FILES"
       for TAR in $FILES
       do
-        jina hub build $TAR
+        jina hub build $TAR || rc=$?
       done
 fi
+exit $rc
