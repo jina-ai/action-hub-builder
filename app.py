73e7cb4cf8802ba3e146e297729b127e503f833b
Han Xiao <han.xiao@jina.ai>
2020-08-26 10:43:44 +0200

    feat: add action yaml

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def main(args):
     for t in all_targets:
         args.path = t
         args.pull = True
+        args.raise_error = True
         s = HubIO(args).build()
         s['path'] = t
         summary['tasks'].append(s)
