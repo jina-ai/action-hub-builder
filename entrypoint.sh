2d8243b861dbf8b1cb97228816960d8c9537eb98
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2020-09-11 18:30:18 +0530

    feat(cd): enabling hub push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
 set -ex
+rc=0
+
+if [ "$#" -ne 9 ]; then
+  echo "Expected 9 parameters. Got $#"
+  echo "./entrypoint.sh IS_CLI DOCKERHUB_USERNAME DOCKERHUB_PASSWORD DOCKERHUB_REGISTRY JINA_DB_HOSTNAME JINA_DB_USERNAME JINA_DB_PASSWORD JINA_DB_NAME JINA_DB_COLLECTION"
+  exit 1
+fi
+
+export IS_CLI=$1
+export DOCKERHUB_USERNAME=$2
+export DOCKERHUB_PASSWORD=$3
+export DOCKERHUB_REGISTRY=$4
+export JINA_DB_HOSTNAME=$5
+export JINA_DB_USERNAME=$6
+export JINA_DB_PASSWORD=$7
+export JINA_DB_NAME=$8
+export JINA_DB_COLLECTION=$9
 
 pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${pull_number}/files"
@@ -9,26 +26,27 @@ FILES=$(curl -s -X GET -G $URL | jq -r '.[] | select( (.filename | endswith("man
 rc=0
 SUCCESS_TARGETS=()
 FAILED_TARGETS=()
-if [ -z "$FILES" ]
-then
-      echo "nothing to build"
+if [ -z "$FILES" ]; then
+    echo "nothing to build"
 else
-      echo "targets to build: $FILES"
-      for TAR_PATH in $FILES
-      do
-        if [[ "$1" == "True" ]];
-           # do ci stuff
+    echo "targets to build: $FILES"
+    for TAR_PATH in $FILES; do
+        if [[ "$IS_CLI" == "True" ]]; then
+            if jina hub build --pull --prune-images --test-uses --raise-error --daemon $TAR_PATH; then
+                SUCCESS_TARGETS+=("$TAR_PATH")
+            else
+                FAILED_TARGETS+=("$TAR_PATH")
+                rc=1
+            fi
         else
-          # do cd stuff
+            if jina hub build --push --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD --registry $DOCKERHUB_REGISTRY --prune-images --raise-error --daemon $TAR_PATH; then
+                SUCCESS_TARGETS+=("$TAR_PATH")
+            else
+                FAILED_TARGETS+=("$TAR_PATH")
+                rc=1
+            fi
         fi
-
-        if jina hub build $TAR_PATH --pull --prune-images --test-uses --raise-error --daemon; then
-          SUCCESS_TARGETS+=("$TAR_PATH")
-        else
-          FAILED_TARGETS+=("$TAR_PATH")
-          rc=1
-        fi
-      done
+    done
 fi
 echo "SUCCESS_TARGETS: $SUCCESS_TARGETS"
 echo "FAILED_TARGETS: $FAILED_TARGETS"
