0ead15f7163449e71f21a97e6b969bf6beae1f05
Han Xiao <han.xiao@jina.ai>
2020-09-14 10:44:30 +0200

    feat: add more options

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ rc=0
 
 if [ "$#" -ne 9 ]; then
   echo "Expected 9 parameters. Got $#"
-  echo "./entrypoint.sh IS_CI DOCKERHUB_USERNAME DOCKERHUB_PASSWORD DOCKERHUB_REGISTRY JINA_DB_HOSTNAME JINA_DB_USERNAME JINA_DB_PASSWORD JINA_DB_NAME JINA_DB_COLLECTION"
+  echo "./entrypoint.sh IS_PUSH DOCKERHUB_USERNAME DOCKERHUB_PASSWORD DOCKERHUB_REGISTRY JINA_DB_HOSTNAME JINA_DB_USERNAME JINA_DB_PASSWORD JINA_DB_NAME JINA_DB_COLLECTION"
   exit 1
 fi
 
-export IS_CI=$1
+export IS_PUSH=$1
 export DOCKERHUB_USERNAME=$2
 export DOCKERHUB_PASSWORD=$3
 export DOCKERHUB_REGISTRY=$4
@@ -31,18 +31,19 @@ if [ -z "$FILES" ]; then
 else
     echo "targets to build: $FILES"
     for TAR_PATH in $FILES; do
-        if [[ "$IS_CI" == "True" ]]; then
-          cmd="jina hub build --pull --prune-images --test-uses --raise-error --daemon $TAR_PATH"
-        else
-          cmd="jina hub build --pulll --prune-images --push --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD --registry $DOCKERHUB_REGISTRY $TAR_PATH;"
-        fi
+      cmd="jina hub build --pull --prune-images"
+      if [[ "$IS_PUSH" == true ]]; then
+        cmd="$cmd --push --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD --registry $DOCKERHUB_REGISTRY $TAR_PATH;"
+      else
+        cmd="$cmd --test-uses --raise-error --daemon $TAR_PATH"
+      fi
 
-        if ($cmd); then
-            SUCCESS_TARGETS+=("$TAR_PATH")
-        else
-            FAILED_TARGETS+=("$TAR_PATH")
-            rc=1
-        fi
+      if ($cmd); then
+          SUCCESS_TARGETS+=("$TAR_PATH")
+      else
+          FAILED_TARGETS+=("$TAR_PATH")
+          rc=1
+      fi
     done
 fi
 echo "SUCCESS_TARGETS: $SUCCESS_TARGETS"
